8f2b508fa12d83bcd24cf448b5bd30a7a1bfbe4c
Roger-Hong <federer.hyj2721@gmail.com>
2019-09-24 00:05:12 -0700

    Add configs for property types and corresponding roles.

---
+++ acl/acl_config.js
@@ -0,0 +1,123 @@
+const PropertyType = {
+  CAMPAIGN: "campaign",
+  CHANNEL: "channel",
+  EVENT: "event",
+  IMAGE: "image",
+  MEMBER: "member",
+  ORGANIZATION: "organization",
+  SESSION: "session",
+};
+
+/*****************
+ * Default roles.
+ *****************/
+const DefaultRoles = {
+  OWNER: "owner",
+  AMDIN: "admin",
+  READ_WRITE: "read_write",
+  READ: "read",
+};
+
+const DefaultAdminRoles = new Map([
+  [DefaultRoles.OWNER, null],
+  [DefaultRoles.AMDIN, DefaultRoles.OWNER],
+  [DefaultRoles.READ_WRITE, DefaultRoles.AMDIN],
+  [DefaultRoles.READ, DefaultRoles.READ_WRITE],
+]);
+
+/*****************
+ * Member roles.
+ *****************/
+const MemberRoles = {
+  OWNER: "owner",
+  AMDIN: "admin",
+  READ_WRITE: "read_write",
+  READ_METADATA: "read_metadata",
+  READ_DETAIL: "read_detail",
+};
+
+const MemberAdminRoles = new Map([
+  [MemberRoles.OWNER, null],
+  [MemberRoles.AMDIN, MemberRoles.OWNER],
+  [MemberRoles.READ_WRITE, MemberRoles.AMDIN],
+  [MemberRoles.READ_METADATA, MemberRoles.READ_WRITE],
+  [MemberRoles.READ_METADATA, MemberRoles.READ_METADATA],
+]);
+
+/*****************
+ * Event roles.
+ *****************/
+const EventRoles = {
+  OWNER: "owner",
+  AMDIN: "admin",
+  READ_WRITE: "read_write",
+  READ: "read",
+  PUBLISH: "publish",
+};
+
+const MemberAdminRoles = new Map([
+  [EventRoles.OWNER, null],
+  [EventRoles.AMDIN, EventRoles.OWNER],
+  [EventRoles.READ_WRITE, EventRoles.AMDIN],
+  [EventRoles.READ, EventRoles.READ_WRITE],
+  [EventRoles.PUBLISH, EventRoles.AMDIN],
+]);
+
+/****************************************************/
+
+const listPropertyTypes = () => {
+  return Object.values(PropertyType);
+}
+
+// Indicates if the acl of the given property type is property based. Otherwise
+// the acl is role bases, whose property ID is empty.
+const isPropertyBased = (propertyType) => {
+  switch (propertyType) {
+    case PropertyType.MEMBER:
+    case PropertyType.ORGANIZATION:
+    case PropertyType.SESSION:
+      return false;
+    default:
+      return true;
+  }
+}
+
+const getRoles = (propertyType) => {
+  switch (propertyType) {
+    case PropertyType.MEMBER:
+      return Object.values(MemberRoles);
+    case PropertyType.EVENT:
+      return Object.values(EventRoles);
+    default:
+      return Object.values(DefaultRoles);
+  }
+}
+
+const getAdminRoles = (propertyType) => {
+  switch (propertyType) {
+    case PropertyType.MEMBER:
+      return Object.values(MemberAdminRoles);
+    case PropertyType.EVENT:
+      return Object.values(MemberAdminRoles);
+    default:
+      return Object.values(DefaultAdminRoles);
+  }
+}
+
+// Find the role above and including targetRole.
+// Return undefined if targetRole and propertyType does not match.
+const getAboveRoles = (propertyType, targetRole) => {
+  const aboveRoles = [];
+  let adminRoleMap = getAdminRoles(propertyType);
+  let role = targetRole;
+  while (adminRoleMap.has(role)) {
+    aboveRoles.push(role);
+    role = adminRoleMap.get(role);
+  }
+}
+
+module.exports = {
+  getPropertyType: getPropertyType,
+  getRoles: getRoles,
+  getAboveRoles: getAboveRoles,
+}
